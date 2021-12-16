f508c9c5b536641d0834988fd8cd11dd73a25373
Peter Smid <petosmid@gmail.com>
2021-12-16 16:14:05 +0100

    add tank shapes

--- Player.js
+++ Player.js
@@ -1,23 +1,69 @@
 module.exports.Player = class Player {
-  constructor(number, x, y, dir){
+  constructor(number, x, y, dir) {
     this.playerNumber = number;
     this.x = x;
     this.y = y;
     this.dir = dir;
+    //prettier-ignore
+    this.tankUp = [
+      0,0,0,1,0,0,0,
+      0,1,0,1,0,1,0,
+      0,1,1,1,1,1,0,
+      0,1,1,1,1,1,0,
+      0,1,1,1,1,1,0,
+      0,1,1,1,1,1,0,
+      0,1,0,0,0,1,0,
+    ]
+    this.tankDown = this.tankUp.slice().reverse();
+    this.tankRight = get90degRotatedOriginalShape(this.tankUp, this.width);
+    this.tankLeft = this.tankRight.slice().reverse();
+    //prettier-ignore
+    this.tankTopRight = [
+      0,0,0,1,0,0,0,
+      0,0,1,1,0,1,0,
+      0,1,1,1,1,0,0,
+      1,1,1,1,1,1,1,
+      0,0,1,1,1,1,0,
+      0,0,0,1,1,0,0,
+      0,0,0,1,0,0,0,
+    ]
+    this.tankBottomRight = get90degRotatedOriginalShape(
+      this.tankTopRight,
+      this.width
+    );
+    this.tankBottomLeft = get90degRotatedOriginalShape(
+      this.tankBottomRight,
+      this.height
+    );
+    this.tankTopLeft = get90degRotatedOriginalShape(
+      this.tankBottomLeft,
+      this.height
+    );
+    this.currentTankShape = this.tankUp;
+
   }
 
-  updateState({x, y, dir}) {
+  updateState({ x, y, dir }) {
     this.x = x;
     this.y = y;
     this.dir = dir;
   }
 
   getState() {
-    return ({
+    return {
       x: this.x,
       y: this.y,
-      dir: this.dir
-    })
+      dir: this.dir,
+    };
   }
+};
 
+function get90degRotatedOriginalShape(matrix, width) {
+  const rotatedTank = [];
+  for (let x = 0; x < width; x++) {
+    for (let y = matrix.length / width - 1; y >= 0; y--) {
+      rotatedTank.push(matrix[[y * width + x]]);
+    }
+  }
+  return rotatedTank;
 }
